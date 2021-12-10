4399914d7afde1a4ea3a8f1ad5e5b35cfb943dc1
sloete <jonas.bwer@gmail.com>
2021-12-10 14:07:33 +0100

    Add basic validation decorators

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,5 +1,14 @@
+import { IsDate, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateEventDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsDate()
+  @Type(() => Date)
   date: Date;
+
+  @IsNotEmpty()
   city: string;
 }
